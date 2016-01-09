53134fa680aa046e1f447b2d44ea1c3c4e7f2c55
Bruno Costa <brunoazev.c@gmail.com>
2016-01-09 12:54:26 -0200

    Configuration - Adjusting route files

--- ci-microservices/ms-feed/app.js
+++ ci-microservices/ms-feed/app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ci_db');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("database connected successfully");
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +19,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +32,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.get('*', function (req, res) {
+    res.send("serving feeds"); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
